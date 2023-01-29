d7e846d3ef7d9c235c888548d168dbb9c4960c54
danij2k <danieldjsp@icloud.com>
2023-01-29 19:10:39 +0000

    first

--- linea temporal/timeline zelda/script.js
+++ linea temporal/timeline zelda/script.js
@@ -1,24 +1,47 @@
+"use stric";
+
 let boxes = document.querySelector(".timeline");
 
 function readJson() {
   // leer fichero y crear un json
   fetch("data.json")
     .then((response) => response.json())
-    //leer los elementos del array
+    //leer los elementos del array y
     .then((data) => {
       data.sort((a, b) => a.date - b.date);
 
-      console.log(data);
-
       data.forEach((element) => {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("content");
-        newDiv.innerHTML = `<div id= textBox><h3></h3>${element.title}</h3><p id= date>${element.date}</p><p>${element.text}</p></div><img src="${element.image}">`;
-        boxes.appendChild(newDiv);
-    });
-
-      console.log(data);
+        createNewContentDiv(
+          element.title,
+          element.text,
+          element.date,
+          element.image
+        );
+      });
     });
 }
 
-window.addEventListener("load", readJson());
+window.addEventListener("load", readJson);
+
+
+let button = document.getElementById("boton");
+button.addEventListener("click", insertGame);
+
+function insertGame() {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const date = document.getElementById("date").value;
+  const image = document.getElementById("image").value;
+  const text = document.getElementById("text").value;
+
+  createNewContentDiv(title, date, text, image);
+}
+
+function createNewContentDiv(title, date, text, image) {
+  console.log(image);
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("content");
+  newDiv.innerHTML = `<div id=textBox><h3></h3>${title}</h3><p id=date>${date}</p><p>${text}</p></div><img src="${image}">`;
+  boxes.appendChild(newDiv);
+  
+}
